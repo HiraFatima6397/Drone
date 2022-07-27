35eeb7663317c263c830c98864c708b835cdea92
HIRA FATIMA <hiraf3141@gmail.com>
2022-07-27 13:38:29 +0530

    complete

--- lib/grid_viewphotos.dart
+++ lib/grid_viewphotos.dart
@@ -10,119 +10,147 @@ class Grid extends StatefulWidget {
 }
 
 class _GridState extends State<Grid> {
-  List<String> item = [
+  List<String> title = [
     StringConstant.phanthom3,
     StringConstant.phanthom4,
     StringConstant.spark,
     StringConstant.mavicPro,
     StringConstant.phanthom3,
     StringConstant.spark,
-
   ];
-   List <String>image = [
-    'https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D106317834757414&imgrefurl=https%3A%2F%2Fwww.facebook.com%2FGetDronedToday%2F&tbnid=PLwu0PmKEdn7PM&vet=12ahUKEwjRqaOHr5b5AhVqg2MGHZBlBfIQMygCegUIARC0AQ..i&docid=gcfWwWXwVWxmmM&w=959&h=958&q=droned%20pics&client=safari&ved=2ahUKEwjRqaOHr5b5AhVqg2MGHZBlBfIQMygCegUIARC0AQ',
-    'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.dronezon.com%2Fwp-content%2Fuploads%2F2014%2F10%2FDJI-Phantom-2-Vision-Plus-e1580563366975.jpg&imgrefurl=https%3A%2F%2Fwww.dronezon.com%2Flearn-about-drones-quadcopters%2Fwhat-is-drone-technology-or-how-does-drone-technology-work%2F&tbnid=SOGeY-x5dHBPiM&vet=12ahUKEwjk94Kdr5b5AhUMgWMGHfp1BO4QMygQegUIARCFAg..i&docid=O371lKdQMQBZuM&w=400&h=263&q=drones%20pics&client=safari&ved=2ahUKEwjk94Kdr5b5AhUMgWMGHfp1BO4QMygQegUIARCFAg',
-    'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww1.djicdn.com%2Fcms%2Fuploads%2Ff2a18ff0b2b0995594596c32bd496ca9.png&imgrefurl=https%3A%2F%2Fwww.dji.com%2F&tbnid=KI2Nntk7UvQv3M&vet=12ahUKEwjk94Kdr5b5AhUMgWMGHfp1BO4QMygTegUIARCLAg..i&docid=HBuv_De7NM-xkM&w=1280&h=1280&q=drones%20pics&client=safari&ved=2ahUKEwjk94Kdr5b5AhUMgWMGHfp1BO4QMygTegUIARCLAg',
-    'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww1.djicdn.com%2Fcms%2Fuploads%2Ff2a18ff0b2b0995594596c32bd496ca9.png&imgrefurl=https%3A%2F%2Fwww.dji.com%2F&tbnid=KI2Nntk7UvQv3M&vet=12ahUKEwjk94Kdr5b5AhUMgWMGHfp1BO4QMygTegUIARCLAg..i&docid=HBuv_De7NM-xkM&w=1280&h=1280&q=drones%20pics&client=safari&ved=2ahUKEwjk94Kdr5b5AhUMgWMGHfp1BO4QMygTegUIARCLAg',
-    'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww1.djicdn.com%2Fcms%2Fuploads%2Ff2a18ff0b2b0995594596c32bd496ca9.png&imgrefurl=https%3A%2F%2Fwww.dji.com%2F&tbnid=KI2Nntk7UvQv3M&vet=12ahUKEwjk94Kdr5b5AhUMgWMGHfp1BO4QMygTegUIARCLAg..i&docid=HBuv_De7NM-xkM&w=1280&h=1280&q=drones%20pics&client=safari&ved=2ahUKEwjk94Kdr5b5AhUMgWMGHfp1BO4QMygTegUIARCLAg',
-    'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww1.djicdn.com%2Fcms%2Fuploads%2Ff2a18ff0b2b0995594596c32bd496ca9.png&imgrefurl=https%3A%2F%2Fwww.dji.com%2F&tbnid=KI2Nntk7UvQv3M&vet=12ahUKEwjk94Kdr5b5AhUMgWMGHfp1BO4QMygTegUIARCLAg..i&docid=HBuv_De7NM-xkM&w=1280&h=1280&q=drones%20pics&client=safari&ved=2ahUKEwjk94Kdr5b5AhUMgWMGHfp1BO4QMygTegUIARCLAg',
-
+  List<String> image = [
+    'https://img.particlenews.com/image.php?type=thumbnail_580x000&url=0VqUrj_0dcjoh4K00',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUuLUPEjTmf4ihNf24mZKKvTbPparxGSV4Gg&usqp=CAU',
+    'https://media.istockphoto.com/photos/quadcopter-drone-with-4k-video-camera-flying-in-the-air-picture-id911190112?k=20&m=911190112&s=612x612&w=0&h=6mkFfZ_h5OVzYH_b0DVaYMdMZd86Zn8MZH89gWPJN0w=',
+    'https://149355317.v2.pressablecdn.com/wp-content/uploads/2021/03/DJI-FPV-Drone-Top-Camera-Drones.jpg',
+    'https://dji-official-fe.djicdn.com/cms/uploads/a49eae1222f1300f4ec5afe38bf90875.png',
+    'https://www.dronezon.com/wp-content/uploads/2014/10/DJI-Phantom-2-Vision-Plus-e1580563366975.jpg',
   ];
-
+  List<String> subtitle = [
+    '\$45',
+    '\$60',
+    '\$75',
+    '\$35',
+    '\$49',
+    '\$99',
+  ];
+  List<String> percent = [
+    '30%',
+    '35%',
+    '40%',
+    '10%',
+    '10%',
+    '6%',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-          child: SafeArea(
+      body: SingleChildScrollView(
+        child: SafeArea(
             child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start, children: [
-                Row(children: const
-                [
-                   Icon(AppIcons.icon),
-                  SizedBox(
-                    width: 110,
-                  ),
-                   Text(StringConstant.drones,
-                      style: TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.bold)),
-                   SizedBox(
-                    width: 100,
-                  ),
-                   Icon(AppIcons.search),
-                ]),
-                const SizedBox(height: 50,),
-                SizedBox(
-                  height: 700,
-
-                  child: GridView.count(
-                      scrollDirection: Axis.vertical,
-                      crossAxisCount: 2,
-                      childAspectRatio: 2,
-                      children: List.generate(image.length, (index){
-                        return _buildCardViewWidget(
-                          imageUrl: image[index],
-                          title: item[index],
-
-                        );
-                      }),
-                  ),
-    ),
-    ],
-    ),
-                )
-              ),
-            ),
-          );
-  }
-
-  _buildCardViewWidget({required String imageUrl,
-    required String title,
-    }) {
-    return Card(
-
-        child: Stack(
-
+          padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  elevation: 10,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(imageUrl
-                          ),
-                          fit: BoxFit.fitWidth,
-                          alignment: Alignment.topCenter,
-                        )),
-                    child: Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Row(
-                          children:const [
-                             Text(
-                              StringConstant.phanthom3,
-                              style: TextStyle(fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                             SizedBox(width: 18,),
-                             Text('45', style:  TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),)
-                          ],
-                        )),
-                  ),
+              Row(children: const [
+                Icon(AppIcons.icon),
+                SizedBox(
+                  width: 110,
+                ),
+                Text(StringConstant.drones,
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                SizedBox(
+                  width: 100,
+                ),
+                Icon(AppIcons.search),
+              ]),
+              const SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                height: 700,
+                child: GridView.count(
+                  scrollDirection: Axis.vertical,
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 15,
+                  children: List.generate(image.length, (index) {
+                    return _buildCardViewWidget(
+                      imageUrl: image[index],
+                      title: title[index],
+                      subtitle: subtitle[index],
+                      percent:percent[index],
+                    );
+                  }),
                 ),
               ),
-              Positioned(left: 30,child: Container(height: 100,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.pink),
-              )
-              )
-            ]));
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+
+  _buildCardViewWidget(
+      {required String imageUrl,
+      required String title,
+      required String subtitle, required String percent}) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 10,
+      child: Stack(children: [
+        Column(children: [
+          Container(
+              height: 120,
+              width: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Image(
+                image: NetworkImage(
+                  imageUrl,
+                ),
+                fit: BoxFit.cover,
+              )),
+          SizedBox(
+            height: 15,
+          ),
+          Align(
+              alignment: Alignment.bottomLeft,
+              child: Row(
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  Text(
+                    subtitle,
+                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                  )
+                ],
+              )),
+        ]),
+        Positioned(
+            left: 115,
+            top: 30,
+            child: Container(
+              height: 35,
+              width: 80,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    topLeft: Radius.circular(20),
+                  ),
+                  color: Colors.red[400]),child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                    child: Text(percent,style: TextStyle(color: Colors.white,fontSize: 16,fontWeight: FontWeight.bold)),
+                  ),
+            ))
+      ]),
+    );
   }
 }
